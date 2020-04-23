0f6e81b43cbaf1641544dc8b0bb6dabe48449b54
AlexMato <matontialessandro0@gmail.com>
2020-04-23 17:19:37 +0200

    aggiunta della funzione per centralizzare

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ $(document).ready(function() {
           $("#search").val("");
           // cancello l'html già esistente in pagina
           $(".contenitor").html("");
-
-
+          // inizio operazioni handlebars
+          var source = $("#entry-template").html();
+          var template = Handlebars.compile(source);
+          // fine operazioni handlebars
 
           console.log(searchMovie);
           // richiamo API per i film
@@ -25,50 +27,18 @@ $(document).ready(function() {
             },
             success: function(data){
               console.log(data.results);
-              // inizio handlebars
-              var source = $("#entry-template").html();
-              var template = Handlebars.compile(source);
+
+              // salvo lista degli oggetti
               var listaFilms = data.results;
-              // fine handlebars
-              for (var i = 0; i < listaFilms.length; i++){
-                console.log(listaFilms[i]);
-                var ogniFilms = listaFilms[i];
 
-
-                // INIZIO OPERAZIONI PER IL VOTO
-                // salvo il voto in una var
-                var voto = ogniFilms.vote_average;
-                console.log("voto del film: " + voto);
-                // divido il voto per 2 portandolo in scala 1 a 5
-                var diviso = voto / 2;
-                console.log("voto del film diviso 2: " + diviso);
-                // arrotondo il risultato a numero intero
-                var stellaPiena = Math.round(diviso);
-                console.log("voto arrotondato: " + stellaPiena);
-                console.log("numero di stelle piene: " + stellaPiena);
-                var stellaVuota = 5 - stellaPiena;
-                console.log("numero di stelle vuote: " +stellaVuota);
-                var stellinaPiena = '<i class="fas fa-star"></i>';
-                var stellinaVuota = '<i class="far fa-star"></i>';
-                // FINE OPERAZIONI PER IL VOTO
-                var context = {
-                    title: ("Titolo del film : " + ogniFilms.title),
-                    origTitle : ("Titolo originale : " + ogniFilms.original_title),
-                    language : "<img src='flags/"+ogniFilms.original_language+".png' alt=''>",
-                    vote : stellinaPiena.repeat(stellaPiena) + stellinaVuota.repeat(stellaVuota)
-                };
-                var risultatoDaAggiungere = template(context);
-                $(".contenitor").append(risultatoDaAggiungere);
-              }
-
+              // richiamo la funzione assegnando la lista degli oggetti ed il parametro per i film
+              generaOutput(listaFilms, "film");
             }
-
-
           })
+          
           // richiamo API per le serie
           $.ajax({
             url : "https://api.themoviedb.org/3/search/tv",
-
             method : "GET",
             data: {
               api_key : "5bb55649ec151524f12d940e4df95515",
@@ -76,48 +46,54 @@ $(document).ready(function() {
               query : searchMovie
             },
             success: function(data){
-              console.log(data.results);
-              // inizio handlebars
-              var source = $("#entry-template").html();
-              var template = Handlebars.compile(source);
+              // salvo lista degli oggetti
               var listaFilms = data.results;
-              // fine handlebars
-              for (var i = 0; i < listaFilms.length; i++){
-                console.log(listaFilms[i]);
-                var ogniFilms = listaFilms[i];
 
-                // INIZIO OPERAZIONI PER IL VOTO
-                // salvo il voto in una var
-                var voto = ogniFilms.vote_average;
-                console.log("voto del film: " + voto);
-                // divido il voto per 2 portandolo in scala 1 a 5
-                var diviso = voto / 2;
-                console.log("voto del film diviso 2: " + diviso);
-                // arrotondo il risultato a numero intero
-                var stellaPiena = Math.round(diviso);
-                console.log("voto arrotondato: " + stellaPiena);
-                console.log("numero di stelle piene: " + stellaPiena);
-                var stellaVuota = 5 - stellaPiena;
-                console.log("numero di stelle vuote: " +stellaVuota);
-                var stellinaPiena = '<i class="fas fa-star"></i>';
-                var stellinaVuota = '<i class="far fa-star"></i>';
-                // FINE OPERAZIONI PER IL VOTO
-
-                var context = {
-                    title: ("Titolo del film : " + ogniFilms.name),
-                    origTitle : ("Titolo originale : " + ogniFilms.original_name),
-                    language : "<img src='flags/"+ogniFilms.original_language+".png' alt=''>",
-                    vote : stellinaPiena.repeat(stellaPiena) + stellinaVuota.repeat(stellaVuota)
-                };
-                var risultatoDaAggiungere = template(context);
-                $(".contenitor").append(risultatoDaAggiungere);
+              // richiamo la funzione assegnando la lista degli oggetti ed il parametro per le serie
+              generaOutput(listaFilms, "tv");
               }
-
-            }
-
-
           })
 
+
+          // creo funzione per stampare in pagina
+          function generaOutput(listaOggetti, tipo) {
+            for (var i = 0; i < listaOggetti.length; i++){
+              // salvo ogni singolo oggetto della lista
+              var ogniOggetto = listaOggetti[i];
+
+              // INIZIO OPERAZIONI PER IL VOTO
+              // salvo il voto in una var
+              var voto = ogniOggetto.vote_average;
+              // divido il voto per 2 portandolo in scala 1 a 5
+              var diviso = voto / 2;
+              // arrotondo il risultato a numero intero
+              var stellaPiena = Math.round(diviso);
+              // salvo il numero di stelle vuote
+              var stellaVuota = 5 - stellaPiena;
+              // salvo l'icona per la stellaVuota e la stellaPiena
+              var stellinaPiena = '<i class="fas fa-star"></i>';
+              var stellinaVuota = '<i class="far fa-star"></i>';
+              // FINE OPERAZIONI PER IL VOTO
+              // creo variabili per titolo e titolo originale
+              var title, originalTitle;
+              if (tipo === "film"){
+                title = ogniOggetto.title;
+                originalTitle = ogniOggetto.original_title;
+              } else if (tipo === "tv"){
+                title = ogniOggetto.name;
+                originalTitle = ogniOggetto.original_name;
+
+              }
+              var context = {
+                  title: ("Titolo del film : " + title),
+                  origTitle : ("Titolo originale : " + originalTitle),
+                  language : "<img src='flags/"+ogniOggetto.original_language+".png' alt=''>",
+                  vote : stellinaPiena.repeat(stellaPiena) + stellinaVuota.repeat(stellaVuota)
+              };
+              var risultatoDaAggiungere = template(context);
+              $(".contenitor").append(risultatoDaAggiungere);
+            }
+          }
 
 
         }
